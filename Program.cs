7696f017e854040d4f70dd550d8dbf2824996724
demendet <thalovar@gmail.com>
2025-04-04 01:45:27 +0200

    thisgood121215163663

--- Program.cs
+++ Program.cs
@@ -3,16 +3,24 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-// Create WebApplication builder.
 var builder = WebApplication.CreateBuilder(args);
+
+// Add console logging.
 builder.Logging.AddConsole();
+
+// Register SignalR services.
 builder.Services.AddSignalR();
 
 var app = builder.Build();
+
+// Map the SignalR hub.
 app.MapHub<CockpitHub>("/sharedcockpithub");
+
 app.Run();
 
+
 // Define AircraftData as a class with public properties.
+// Note: ParkingBrake is an int (acting as a boolean), and we add IAS fields.
 public class AircraftData
 {
     public double Latitude { get; set; }
@@ -27,18 +35,19 @@ public class AircraftData
     public double Rudder { get; set; }
     public double BrakeLeft { get; set; }
     public double BrakeRight { get; set; }
-    public int ParkingBrake { get; set; } // ParkingBrake as int (represents bool)
+    public int ParkingBrake { get; set; } // Represents a boolean (0 or 1)
     public double Mixture { get; set; }
     public int Flaps { get; set; }
     public int Gear { get; set; }
-    public double IndicatedAirspeed { get; set; } // new field (in knots)
-    public double TrueAirspeed { get; set; }        // new field (in knots)
+    public double IndicatedAirspeed { get; set; } // In knots
+    public double TrueAirspeed { get; set; }        // In knots
 }
 
 // SignalR Hub for broadcasting flight data.
 public class CockpitHub : Microsoft.AspNetCore.SignalR.Hub
 {
     private readonly ILogger<CockpitHub> _logger;
+
     public CockpitHub(ILogger<CockpitHub> logger)
     {
         _logger = logger;
